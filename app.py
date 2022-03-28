d873ccdc8f3a84e6b3c0d79ed9e6dd33bc60d86a
pho3nix <hisyam001@e.ntu.edu.sg>
2022-03-28 12:19:30 +0800

    Initial Commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from urllib.robotparser import RequestRate
+from flask import Flask, render_template, request 
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/videolist')
+def videolist():
+    return render_template('videolist.html')
+
+@app.errorhandler(404) 
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
